53acf3d5b42a3190bca40db9f43a4c63c4352727
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-05-25 08:35:59 -0500

    added a few route packages

---
+++ controllers/api/notificationRoutes.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+const { Notification } = require("../../models");
+
+
+// GET /api/notifications
+router.post("/", async (req, res) => {
+  try {
+    const newNotification = await Notification.create({
+      item_id: req.body.item_id,
+      item_owner: req.body.item_owner,
+      found_user: req.body.found_user,
+    });
+
+    res.status(200).json(newNotification);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+// DELETE /api/notifications/:id
+router.delete("/:id", async (req, res) => {
+  try {
+    const Notification = await Notification.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(Notification);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
