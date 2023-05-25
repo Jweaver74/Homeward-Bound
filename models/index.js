f3d9adaf497bdda66088afa9c0b76a87ef681133
JerrickJohnson <jerrickjohnson88@gmail.com>
2023-05-25 18:31:06 -0500

    Adding Models

---
+++ models/index.js
@@ -0,0 +1,31 @@
+const User = require('./user');
+const Pet = require('./pets');
+const Notification = require('./Notification');
+
+User.hasMany(Pet, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+User.hasMany(Notification, {
+  foreignKey: 'pet_owner',
+  onDelete: 'CASCADE',
+});
+
+Pet.hasOne(Notification, {
+  foreignKey: 'pet_id',
+});
+
+Pet.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Notification.belongsTo(User, {
+  foreignKey: 'pet_owner',
+});
+
+Notification.belongsTo(Pet, {
+  foreignKey: 'pet_id',
+});
+
+module.exports = { User, Pet, Notification };
